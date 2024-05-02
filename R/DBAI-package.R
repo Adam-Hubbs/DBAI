59f054aa3e7ea250d42a8cc0ab98c07542539aca
Adam-Hubbs <amhubbs17@gmail.com>
2024-05-02 08:49:41 -0600

    skeleton

---
+++ R/DBAI-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr mutate
+#' @importFrom openai create_chat_completion
+#' @importFrom rlang :=
+#' @importFrom tibble is_tibble
+## usethis namespace: end
+NULL
